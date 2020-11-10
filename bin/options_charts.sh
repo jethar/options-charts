fa5c9432e6c144c7a30cb3acdf38afc74b61e16e
Harjeet Singh <harjeetss@harjeet.com>
2020-11-10 09:51:45 +0530

    delete env file

--- bin/options_charts.sh
+++ bin/options_charts.sh
@@ -3,5 +3,6 @@
 cd /home/harjeet/options-charts
 source venv/bin/activate
 source <(grep -v '^#' .env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
+rm -rf .env
 cd /home/harjeet/options-charts/src/options_charts
 python ticker.py
