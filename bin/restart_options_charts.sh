632f5c84a814fab7d35338ddfc46d83e513416aa
Harjeet Singh <harjeetss@harjeet.com>
2020-11-19 23:21:47 +0530

    updating restart

--- bin/restart_options_charts.sh
+++ bin/restart_options_charts.sh
@@ -5,7 +5,7 @@ if [ $(ps -ef | grep ticker | wc -l) < 2 ]
  	cd /home/harjeet/options-charts
 	source venv/bin/activate
 	source <(grep -v '^#' .env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
-	rm -rf .env
+	# rm -rf .env
 	cd /home/harjeet/options-charts/src/options_charts
 	python ticker_fullmode.py
 fi
