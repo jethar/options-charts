61c5ac568069bdd9746e5fa8da6007eb4e7d4a70
Harjeet Singh <harjeetss@harjeet.com>
2020-11-19 23:16:03 +0530

    providding restart option for cronjob

--- bin/reset_options_charts.sh
+++ bin/reset_options_charts.sh
@@ -2,6 +2,7 @@
 
 cd /home/harjeet/options-charts
 source venv/bin/activate
+kill $(pgrep python)
 source <(grep -v '^#' .env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
 # rm -rf .env
 cd /home/harjeet/options-charts/src/options_charts
--- bin/restart_options_charts.sh
+++ bin/restart_options_charts.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd /home/harjeet/options-charts
-source venv/bin/activate
-kill $(pgrep python)
-source <(grep -v '^#' .env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
-rm -rf .env
-cd /home/harjeet/options-charts/src/options_charts
-python ticker_fullmode.py
+if [ $(ps -ef | grep ticker | wc -l) < 2 ]
+ then
+ 	cd /home/harjeet/options-charts
+	source venv/bin/activate
+	source <(grep -v '^#' .env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
+	rm -rf .env
+	cd /home/harjeet/options-charts/src/options_charts
+	python ticker_fullmode.py
+fi
