998a4abf0cf3649a26cb5c0e82c352c47dfedf9a
Harjeet Singh <harjeetss@harjeet.com>
2020-11-20 12:00:49 +0530

    restart_options-Charts script updated

--- bin/archive_ticks.sh
+++ bin/archive_ticks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+rm -rf /home/harjeet/options-charts/.env
 cd /home/harjeet/options-charts/data
 tar czf "ticks_$(date +"%Y-%m-%d").db.tar.gz" "ticks_$(date +"%Y-%m-%d").db"
+
+# cd /home/harjeet/options-charts/data; gsutil cp -R ticks_$(date +"%Y-%m-%d").db.tar.gz gs://finmkt-tick-data/2020/11/
--- bin/restart_options_charts.sh
+++ bin/restart_options_charts.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ $(ps -ef | grep ticker | wc -l) < 2 ]
+if [[ $(ps -ef | grep ticker | wc -l) -lt 2 ]]
  then
  	cd /home/harjeet/options-charts
 	source venv/bin/activate
 	source <(grep -v '^#' .env | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
+	touch RESTARTED_$(date +"%Y-%m-%d_%H-%M-%S")
 	# rm -rf .env
 	cd /home/harjeet/options-charts/src/options_charts
 	python ticker_fullmode.py
